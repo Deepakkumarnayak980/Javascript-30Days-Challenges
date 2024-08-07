9d8fa90ba86ddb202a31c881dd799b92c885173d
Deepakkumarnayak980 <123307079+Deepakkumarnayak980@users.noreply.github.com>
2024-08-08 00:55:51 +0530

    day-7 practices

--- Day-7/Day-7.js
+++ Day-7/Day-7.js
@@ -32,40 +32,40 @@ books.books.forEach(book => {
   });
   
 // Task-3 //
-// const library = {
-//     name: "City Library",
-//     books: [
-//       {
-//         title: "To Kill a Mockingbird",
-//         author: "Harper Lee",
-//         year: 1960,
-//         getDetails() {
-//           return `Title: ${this.title}, Author: ${this.author}`;
-//         }
-//       },
-//       {
-//         title: "1984",
-//         author: "George Orwell",
-//         year: 1949,
-//         getDetails() {
-//           return `Title: ${this.title}, Author: ${this.author}`;
-//         }
-//       },
-//       {
-//         title: "Moby Dick",
-//         author: "Herman Melville",
-//         year: 1851,
-//         getDetails() {
-//           return `Title: ${this.title}, Author: ${this.author}`;
-//         }
-//       }
-//     ]
-//   };
+const library = {
+    name: "City Library",
+    books: [
+      {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960,
+        getDetails() {
+          return `Title: ${this.title}, Author: ${this.author}`;
+        }
+      },
+      {
+        title: "1984",
+        author: "George Orwell",
+        year: 1949,
+        getDetails() {
+          return `Title: ${this.title}, Author: ${this.author}`;
+        }
+      },
+      {
+        title: "Moby Dick",
+        author: "Herman Melville",
+        year: 1851,
+        getDetails() {
+          return `Title: ${this.title}, Author: ${this.author}`;
+        }
+      }
+    ]
+  };
   
-//   // Logging the result of calling the getDetails method
-//   library.books.forEach(book => {
-//     console.log(book.getDetails());
-//   });
+  // Logging the result of calling the getDetails method
+  library.books.forEach(book => {
+    console.log(book.getDetails());
+  });
   
 
 //Task-4//
@@ -76,4 +76,59 @@ books.updateYear = function (newYear) {
 console.log("\nBefore updating book's publish year is: ", books.year);
 books.updateYear(2000);
 console.log("After updating book's publish year is: ", books.year, "\n");
+
+
+
+
+// task-5//
+
+const library ={
+  name: "city library",
+  books:[
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+    }
+  ,{
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+  }
+  ]
+}
+
+console.log(library);
+
+
+// Task-6 //
+
+const nameOfLibrary = library.name;
+console.log("\nThe name of the library is:",nameOfLibrary);
+
+// Yask -7 //
+const book = {
+  title: "The 48 Laws of Power",
+  author: "Robert Greene",
+  year: 1998,
+};
+book.details = function () {
+  return `Book Title: ${this.title}\nBook Author: ${this.author}`;
+};
+console.log("\n" + book.details());
+
+
+// Task-8 //
+console.log("\nIterating over the properties of the book object using for...in");
+for (const key in book) {
+    console.log(`${key}: ${book[key]}`);
+}
+
+// --> task - 9 <--
+console.log("\nUsing Object.keys and Object.values methods to log all the keys and values of the book object.");
+console.log("\nAll the keys of the book object are: ", Object.keys(book));
+console.log("\nAll the values of the book object are: ", Object.values(book));
 
